842f6d49d5ac7f85f2b70fb65353327d918d6920
beneftwvu <bhe00002@mix.wvu.edu>
2026-02-16 15:29:12 -0500

    Fixed includes in main()

--- src/main.c
+++ src/main.c
@@ -2,9 +2,33 @@
 #include <stdio.h>
 #include "simulation/simulator_ui.h"
 
-int main(void) {
+#include "flight_software/controllers/generic_controller.h"
+#include "flight_software/drivers/generic_driver.h"
+#include "simulation/mounts/generic_mount.h"
 
-    printf( "\nInitializing UI...\n" );
+int main(void)
+{
+
+    generic_driver_t generic_driver;
+
+    driver_operations_t ops = {
+        .init = NULL,
+        .sample = generic_sample,
+        .calibrate = NULL,
+        .emergency = NULL,
+        .cleanup = NULL,
+        .sample_size = sizeof(double)};
+
+    generic_driver_init(
+        &generic_driver,
+        "Generic Driver",
+        1,
+        &ops,
+        NULL,
+        &instrument_generic
+    );
+
+    printf("\nInitializing UI...\n");
     initialize_UI();
     main_menu();
     return 1;
